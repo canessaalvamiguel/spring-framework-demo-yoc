e62ba3d886a3edf40fe6ea26241616a48096af0a
Miguel Canessa <canessaalvamiguel@gmail.com>
2021-09-21 00:25:54 -0500

    Add sb admin template to static directory

---
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -0,0 +1,13 @@
+package com.cursojava.curso.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController {
+
+    @RequestMapping(value = "prueba")
+    public String prueba(){
+        return "prueba";
+    }
+}
