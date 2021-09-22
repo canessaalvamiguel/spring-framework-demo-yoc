2ac62b1454ebb2080b31409116840a0e4e8169a0
Miguel Canessa <canessaalvamiguel@gmail.com>
2021-09-22 02:00:27 -0500

    Add user model

---
+++ src/main/java/com/cursojava/curso/models/User.java
@@ -0,0 +1,58 @@
+package com.cursojava.curso.models;
+
+public class User {
+    private Long id;
+    private String name;
+    private String lastname;
+    private String email;
+    private String telephone;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
